f5eca9609e2c7f61ca0516e2b029bdeb3d4e0732
Max Horstmann <max.horstmann@gmail.com>
2014-10-08 22:07:09 -0400

    gobox

---
+++ gobox.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	fmt.Println("listing...")
+	err := http.ListenAndServe(":1234", nil)
+
+}
+
+func rootHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(
+}
